1d9a69a033ab1486bf6ff0d4ef87cea98bdfcd99
Belen Garcia Martinez <93bgarciamartinez@gmail.com>
2023-01-20 06:36:23 -0600

    onclick event added to linkedin button

--- src/components/buttons/linkedin.js
+++ src/components/buttons/linkedin.js
@@ -2,5 +2,11 @@ import { Button } from "react-bootstrap"
 import "./buttons.css";
 
 export const Linkedin = () => {
-    return <Button id='linkedin' variant='outline-info'>LinkedIn</Button>
+
+    const takeToLinkedin = () => {
+        window.open('https://www.linkedin.com/in/93belen/')
+    }
+
+
+    return <Button onClick={takeToLinkedin} onTouchStart={takeToLinkedin} id='linkedin' variant='outline-info'>LinkedIn</Button>
 }
