34bf41f8aee4f531d1164b65a2d4e17254a1ab1c
Belen Garcia Martinez <93bgarciamartinez@gmail.com>
2023-01-25 06:57:34 -0600

    tweak about content

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -1,4 +1,5 @@
 import { Container, Image } from "react-bootstrap"
+import {Linkedin} from '../buttons/linkedin'
 import './about.css'
 import '../../index.css';
 
@@ -6,18 +7,23 @@ export const About = () => {
     return (
         <Container id='about'>
             <Container id='textContainer'>
-                <p>A few years ago, I moved to <span className='span'>London</span>, not knowing a word of English and having almost no work experience.</p>
-                <p>Two years later, I was <span className='span'>fluent in English</span> and had developed a strong work ethic and a career.</p>
-                
-                <p>During the lock-down in <span className='span'>Spain</span>, I stumbled upon coding, and haven't stopped building projects since.</p>
-
-                <p>Now, I see how my education in <span className='span'>philosophy</span> prepared me to understand complex systems, find problems within them, and <span className='span'>solve them</span> by thinking logically and creatively.</p>
-                <p>Becoming bilingual has made it easy for me to <span className='span'>learn new languages</span>.</p>
-
-                <p>While, moving around the world and working in retail, have shaped me to be <span className='span'>driven</span>, learn fast, work hard, understand people, and be a team player.</p>
-
-                <p>I have completed a couple of courses, but most importantly, I spend endless hours learning, coding, and researching.</p>
-                <p><span id='last-sentence' className='span'>I just love building stuff</span>.</p>
+            <h1 id='h1'> Hi! My name is Belen, a Software Developer from Spain, currently living in Kansas City.</h1>
+            <p>After studying <span className='span'>Philosophy</span> at the University of Granada, I moved to <span className='span'>London</span>.</p>
+            <p>Looking for opportunities to better my life, I flee the economic struggles and work desert of <span className='span'>Spain</span>, not knowing a word of English and having almost no work experience.</p>
+            <p>Two years later, I was fluent in <span className='span'>English</span> and had developed a strong work ethic and a career in retail.</p>
+            <p>A few months before Covid hit, I moved back to my country, as many immigrants did, worried about the crisis that <span className='span'>Brexit</span> might bring to England.</p>
+            <p>During the Lock-down in Spain, I stumbled upon <span className='span'>coding</span>, and haven't stopped building projects since.</p>
+            <p>But, after a couple of years, desapointed with how little the economic situation had improved in <span className='span'>Spain</span>, 
+            and having to survive with little more than $600 a month, my husband and I moved to <span className='span'>America</span>, where he grew up.</p>
+            <p>Here, hard work pays off, and it has taken me little less than a year to prepare myself for a carreer in <span className='span'>Tech</span>.</p>
+            <p>Now, I see how my education in philosophy prepared me to understand <span className='span'>complex systems</span>, 
+            find problems within them, and solve them by thinking <span className='span'>logically</span> and creatively.</p>
+            <p>Becoming bilingual has made it easy for me to learn new <span className='span'>languages</span>.</p>
+            <p>While, moving around the <span className='span'>world</span> and working in retail, have shaped me to be driven, learn fast, work hard, understand people, and be a team player.</p>
+            <p>I have completed a couple of courses, but most importantly, I spend endless hours learning, coding, and researching.</p>
+            <p><span className='span'>I just love building stuff</span>.</p>
+            <p>I am seeking a full-time position as a <span className='span'>React Developer</span>. Connect with my in LinkedIn!</p>
+            <Linkedin />
             </Container>
             <Image id='img-amercia' src={require('../../imgs/america.png')} />
         </Container>
