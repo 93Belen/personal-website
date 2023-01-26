4dd106c9c91ec6208d7d2b95439671694cb36034
Belen Garcia Martinez <93bgarciamartinez@gmail.com>
2023-01-26 15:09:36 -0600

    changed email

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -11,7 +11,7 @@ export const Contact = () => {
                 <Container id='text-container'>
                 <p>&#128075; Hey! If you need a Software Developer pasionated about the craft<br/>
                     I am currently looking for a position as a <span className='span'>React Developer</span><br/>
-                    Contact me via email at <span className='span'>93bgarciamartinez@gmail.com</span><br/>
+                    Contact me via email at <span className='span'>belengm.dev@gmail.com</span><br/>
                     Call me at <span className='span'>913-274-0955</span><br/>
                     And dont forget to <span className='span'>connect with me</span><br/>
                     <Linkedin />
